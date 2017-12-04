443668ab258c8a1631c66af661e762769d4824c6
luhua187 <luhua187@126.com>
2017-12-04 23:09:15 +0800

    第15题

---
+++ 15_3sum.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void quiksort(int *a, int left, int right)
+{
+	if(left >= right)
+		return ;
+
+	int i = left, j = right, key = a[left];
+
+	while( i < j)
+	{
+		while(j > i && a[j] >= key)
+		{
+			j--;
+		}
+		a[i] = a[j];
+
+		while(i < j && a[i] <= key)
+		{
+			i++;
+		}
+		a[j] = a[i];
+	}
+
+	a[i] = key;
+
+	quiksort(a, left, i-1);
+	quiksort(a, i+1, right);
+}
+
+
+int search(int *a,int l, int r, int value)
+{  
+    int low = l, high = r, mid;  
+    while (low <= high) 
+    {  
+        mid = (low + high)/2;  
+        if (value > a[mid]) 
+        {  
+            low = mid+1;  
+        }
+        else if (value < a[mid])
+        {  
+            high = mid - 1;  
+        }
+        else
+        {  
+            return mid;  
+        }  
+    } 
+
+    return -1;  
+}
+
+
+int** threeSum(int* nums, int numsSize, int* returnSize)
+{
+	int i, j, s, r, t = 0, m, n =0;
+	int **p = (int **)malloc(sizeof(int*) * 50000);
+
+	quiksort(nums, 0, numsSize-1);
+
+	for(i = 0; i < numsSize && nums[i] < 0; i++)
+	{
+
+		for(j = i+1; j < numsSize; j++)
+		{		
+			s = 0 - nums[i] - nums[j];
+			r = search(nums, j+1, numsSize-1, s);
+			printf("%d \n", r);
+			if(r != -1)
+			{
+				n = 0;
+				for(m = 0; m < t; m++)
+				{
+					if(p[m][0]==nums[i] && p[m][1]==nums[j] && p[m][2]==nums[r] )
+					{	n = 1;  break;}
+				}
+				if(n)
+					continue;
+
+				p[t]    = (int *)malloc(sizeof(int) * 3);
+				p[t][0] = nums[i];
+				p[t][1] = nums[j];
+				p[t][2] = nums[r];
+				t++;
+				printf("%d %d %d\n", nums[i], nums[j],nums[r]);
+			}
+
+		}
+	}
+
+	*returnSize = t;
+    return p;
+}
+
+int main()
+{
+	int i, r;
+	//int a[] = {-7,-4,-6,6,4,-6,-9,-10,-7,5,3,-1,-5,8,-1,-2,-8,-1,5,-3,-5,4,2,-5,-4,4,7};
+	int a[] = {0,0,0};
+
+	threeSum(a, 3, &r);
+
+	return 0;
+}
