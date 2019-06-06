9aefe59250f06a91108c1045ddeb2a3c034b975a
luhua187 <luhua187@126.com>
2019-06-06 22:17:51 +0800

    112 path sum

---
+++ 112_path_sum.py
@@ -0,0 +1,21 @@
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if root == None:
+            return False
+        if root.left==None and root.right==None and root.val == sum:
+            return True
+        elif root.left==None and root.right==None and root.val != sum:
+            return False
+        else:
+            return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
